ed6e0344511366fd775dcd1da0d87949742c872d
ChristopherKing <jinl1037@hotmail.com>
2017-02-28 22:30:33 +0800

    complete problem 3A.

--- project/ants/ants.py
+++ project/ants/ants.py
@@ -88,6 +88,7 @@ class Insect(object):
 
     is_ant = False
     damage = 0
+    watersafe = False
 
     def __init__(self, armor, place=None):
         """Create an Insect with an armor amount and a starting Place."""
@@ -123,6 +124,7 @@ class Bee(Insect):
 
     name = 'Bee'
     damage = 1
+    watersafe = True
 
     def sting(self, ant):
         """Attack an Ant, reducing the Ant's armor by 1."""
@@ -173,7 +175,7 @@ class HarvesterAnt(Ant):
     food_cost = 2
 
     def __init__(self, armor=1):
-        Ant.__ini__(self, armor=1)
+        Ant.__init__(self, armor=1)
 
 
     def action(self, colony):
@@ -228,11 +230,13 @@ def random_or_none(s):
 class Water(Place):
     """Water is a place that can only hold 'watersafe' insects."""
 
+
     def add_insect(self, insect):
         """Add insect if it is watersafe, otherwise reduce its armor to 0."""
         # BEGIN Problem 3A
-        "*** REPLACE THIS LINE ***"
-        # END Problem 3A
+        Place.add_insect(self, insect)
+        if not insect.watersafe:
+            insect.reduce_armor(insect.armor)
 
 
 class FireAnt(Ant):
