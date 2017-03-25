937c9d25ec8d0207d64a1511376d10db459e409a
ChristopherKing <jinl1037@hotmail.com>
2017-03-25 14:29:02 +0800

    complete lab12.

--- lab/lab12/lab12.sql
+++ lab/lab12/lab12.sql
@@ -3,30 +3,31 @@
 -- Q2
 CREATE TABLE obedience as
   -- REPLACE THIS LINE
-  SELECT 'YOUR CODE HERE';
+  SELECT seven, denero FROM students;
 
 
 -- Q3
 CREATE TABLE blue_dog as
   -- REPLACE THIS LINE
-  SELECT 'YOUR CODE HERE';
+  SELECT color, pet FROM students where color = 'blue' and pet = 'dog';
 
 
 -- Q4
 CREATE TABLE smallest_int as
   -- REPLACE THIS LINE
-  SELECT 'YOUR CODE HERE';
+  SELECT time, smallest From students where smallest > 6 ORDER BY smallest LIMIT 20;
 
 
 -- Q5
 CREATE TABLE sevens as
   -- REPLACE THIS LINE
-  SELECT 'YOUR CODE HERE';
+  SELECT s.seven FROM students as s, checkboxes as c WHERE s.number = 7 and c.'7' = 'True' and s.time = c.time;
 
 
 -- Q6
 CREATE TABLE matchmaker as
   -- REPLACE THIS LINE
-  SELECT 'YOUR CODE HERE';
+  SELECT first.pet, first.song, first.color, second.color FROM students as first, students as second
+    WHERE first.time < second.time and first.pet = second.pet and first.song = second.song;
 
 
